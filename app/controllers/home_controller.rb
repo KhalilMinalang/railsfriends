6210637ad34ad05aa727bab85f501bc50431f0aa
Khalil Minalang <minalang.khalil@gmail.com>
2023-06-19 18:49:53 +0800

    updates

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  def index
+    # code below will return a JSON response instead of a view
+    # json_response = { message: 'Hello, world' }
+    # render json: json_response
+  end
+end
